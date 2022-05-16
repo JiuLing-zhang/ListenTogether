491b45dda7df1c3c25244628970bd09d51498eeb
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-05-16 18:38:43 +0800

    1、win平台下窗口大小简单的适配下不同分辨率

--- src/ListenTogether/MauiProgram.cs
+++ src/ListenTogether/MauiProgram.cs
@@ -44,25 +44,40 @@ namespace ListenTogether
                         WindowId win32WindowsId = Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
                         AppWindow appWindow = AppWindow.GetFromWindowId(win32WindowsId);
 
-                        const int width = 1100;
-                        const int height = 650;
                         Microsoft.UI.Windowing.DisplayArea displayArea = Microsoft.UI.Windowing.DisplayArea.GetFromWindowId(win32WindowsId, Microsoft.UI.Windowing.DisplayAreaFallback.Nearest);
+                        
+                        //简单适配不同分辨率
+                        int minWidth;
+                        int minHeight;
+                        if (displayArea.WorkArea.Width>2000)
+                        {
+                            minWidth=1650;
+                            minHeight=975;
+                        }else if (displayArea.WorkArea.Width>1000)
+	                    {
+                            minWidth=1100;
+                            minHeight=650;
+	                    }else
+                        {
+                            minWidth=880;
+                            minHeight=520;
+                        }
 
                         appWindow.Changed+=(aw,e)=>{
                             if(!e.DidSizeChange)
                             {
                                 return;
                             }
-                            if (aw.Size.Width<width)
+                            if (aw.Size.Width<minWidth)
 	                        {
-                                appWindow.Resize(new SizeInt32(width, aw.Size.Height));
+                                appWindow.Resize(new SizeInt32(minWidth, aw.Size.Height));
 	                        }
-                            if (aw.Size.Height<height)
+                            if (aw.Size.Height<minHeight)
 	                        {
-                                appWindow.Resize(new SizeInt32(aw.Size.Width, height));
+                                appWindow.Resize(new SizeInt32(aw.Size.Width, minHeight));
 	                        }
                         };
-                        appWindow.MoveAndResize(new RectInt32(displayArea.WorkArea.Width / 2 - width / 2, displayArea.WorkArea.Height / 2 - height / 2, width, height));
+                        appWindow.MoveAndResize(new RectInt32(displayArea.WorkArea.Width / 2 - minWidth / 2, displayArea.WorkArea.Height / 2 - minHeight / 2, minWidth, minHeight));
                     });
                 });
             });
