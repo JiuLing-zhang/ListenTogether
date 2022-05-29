80a1ffeeca04841e83fd41d1ca7a2855614245f4
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-05-29 22:03:38 +0800

    1、新增IOS和Mac的高斯模糊分部类，不然项目无法通过编译

---
+++ src/ListenTogether/Platforms/iOS/GaussianImageHandler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Handlers;
+
+namespace ListenTogether.Handlers.GaussianBlurHandler;
+internal partial class GaussianImageHandler : ViewHandler<IGaussianImage, UIKit.UIView>
+{
+    protected override UIKit.UIView CreatePlatformView()
+    {
+        throw new NotImplementedException();
+    }
+
+    static async void MapUrl(GaussianImageHandler handler, IGaussianImage gaussianBlurImage)
+    {
+        throw new NotImplementedException();
+    }
+}
