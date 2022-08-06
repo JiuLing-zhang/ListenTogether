a4fb99f5304cced2e1f1638ee3119989cc7a79be
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-08-06 18:05:27 +0800

    1、删除不在使用的服务

--- src/ListenTogether/Services/ServicesExtensions.cs
+++ src/ListenTogether/Services/ServicesExtensions.cs
@@ -6,13 +6,6 @@ internal static class ServicesExtensions
 {
     public static MauiAppBuilder ConfigureServices(this MauiAppBuilder builder)
     {
-
-#if WINDOWS
-        builder.Services.AddSingleton<IAudioService, ListenTogether.Platforms.Windows.AudioService>();      
-#elif ANDROID
-        builder.Services.AddSingleton<IAudioService, ListenTogether.Platforms.Android.AudioService>();
-#endif
-
         builder.Services.AddTransient<WifiOptionsService>();
         builder.Services.AddSingleton<PlayerService>();
         builder.Services.AddSingleton<HttpClientHelper>();
