d484b0adb88bed76619f3a391fe46501d4677b5b
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-08-18 14:23:21 +0800

    1、Shell中删除播放页

--- src/ListenTogether/Pages/PagesExtensions.cs
+++ src/ListenTogether/Pages/PagesExtensions.cs
@@ -4,12 +4,12 @@ public static class PagesExtensions
 {
     public static MauiAppBuilder ConfigurePages(this MauiAppBuilder builder)
     {
-        builder.Services.AddTransient<MyFavoritePage>();
-        builder.Services.AddSingleton<PlayingPage>();
+        builder.Services.AddTransient<MyFavoritePage>();    
         builder.Services.AddSingleton<PlaylistPage>();
         builder.Services.AddTransient<SearchResultPage>();
         builder.Services.AddSingleton<SettingsPage>();
 
+        builder.Services.AddSingleton<PlayingPage>();
         builder.Services.AddTransient<MyFavoriteDetailPage>();
         builder.Services.AddSingleton<LoginPage>();
         builder.Services.AddSingleton<RegisterPage>();
--- src/ListenTogether/ViewModels/ShellViewModel.cs
+++ src/ListenTogether/ViewModels/ShellViewModel.cs
@@ -27,7 +27,6 @@ internal class ShellViewModel : ViewModelBase
     public AppSection SearchResult { get; set; }
     public AppSection Playlist { get; set; }
     public AppSection MyFavorite { get; set; }
-    public AppSection Playing { get; set; }
     public AppSection Settings { get; set; }
 
     public ShellViewModel()
@@ -35,7 +34,6 @@ internal class ShellViewModel : ViewModelBase
         SearchResult = new AppSection() { Title = "搜索", Icon = "search.png", IconDark = "search_dark.png", TargetType = typeof(SearchResultPage) };
         Playlist = new AppSection() { Title = "播放列表", Icon = "playlist.png", IconDark = "playlist_dark.png", TargetType = typeof(PlaylistPage) };
         MyFavorite = new AppSection() { Title = "我的歌单", Icon = "heart.png", IconDark = "heart_dark.png", TargetType = typeof(MyFavoritePage) };
-        Playing = new AppSection() { Title = "正在播放", Icon = "music.png", IconDark = "music_dark.png", TargetType = typeof(PlayingPage) };
         Settings = new AppSection() { Title = "我的", Icon = "my.png", IconDark = "my_dark.png", TargetType = typeof(SettingsPage) };
     }
 }
