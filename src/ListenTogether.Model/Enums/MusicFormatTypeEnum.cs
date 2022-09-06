64b1cbcac0f1b58c444f478957d60a2d97689ea6
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-09-06 21:47:06 +0800

    1、优化命名

--- src/ListenTogether.Model/Enums/MusicFormatTypeEnum.cs
+++ src/ListenTogether.Model/Enums/MusicFormatTypeEnum.cs
@@ -6,12 +6,12 @@ public enum MusicFormatTypeEnum
     [Description("普通")]
     PQ = 1,
 
-    [Description("高品质")]
+    [Description("高级")]
     HQ = 3,
 
-    [Description("超高品质")]
+    [Description("超高")]
     SQ = 5,
 
-    [Description("比超高还高，应该叫啥？")]
+    [Description("极致")]
     ZQ = 7
 }
