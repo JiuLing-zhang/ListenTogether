2114542007eadb795612df963b74e2558edc0ec7
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-10-22 22:23:21 +0800

    1、新增搜索历史记录的存储模块

---
+++ src/ListenTogether.Storage/SearchHistoryStorage.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+
+namespace ListenTogether.Storage;
+public class SearchHistoryStorage
+{
+    public static void Add(string key)
+    {
+        key = key.ToLower();
+        if (string.IsNullOrEmpty(key))
+        {
+            return;
+        }
+
+        var histories = GetHistories();
+        if (histories.Contains(key))
+        {
+            return;
+        }
+
+        histories.Add(key);
+        SetHistories(JsonSerializer.Serialize(histories));
+    }
+
+    public static IList<string> GetHistories()
+    {
+        string json = Preferences.Get("Histories", "");
+        if (string.IsNullOrEmpty(json))
+        {
+            return new List<string>();
+        }
+        return JsonSerializer.Deserialize<List<string>>(json);
+    }
+
+    public static void Clear()
+    {
+        SetHistories(JsonSerializer.Serialize(new List<string>()));
+    }
+
+    private static void SetHistories(string histories)
+    {
+        Preferences.Set("Histories", histories);
+    }
+}
