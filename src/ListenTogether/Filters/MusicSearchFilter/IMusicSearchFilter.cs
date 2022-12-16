5de930b876330db0f0766e764e9619c84d8b4ee7
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-12-16 10:54:58 +0800

    test

--- src/ListenTogether/Filters/MusicSearchFilter/IMusicSearchFilter.cs
+++ src/ListenTogether/Filters/MusicSearchFilter/IMusicSearchFilter.cs
@@ -1,11 +1,5 @@
 ﻿namespace ListenTogether.Filters.MusicSearchFilter;
-//test4
-//test 2
-//test 3
-//test 5 
-// test 6
-//test 7
-//test 8
+
 internal interface IMusicSearchFilter
 {
     public List<MusicSearchResult> Filter(List<MusicSearchResult> musics);
