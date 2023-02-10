0091a4a364f44acce1bbe46e0a72b8e6f10c6c49
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2023-02-10 19:41:36 +0800

    删除不再使用的模型

--- src/ListenTogether.Api/DbContext/DataContext.cs
+++ src/ListenTogether.Api/DbContext/DataContext.cs
@@ -5,10 +5,8 @@ namespace ListenTogether.Api.DbContext;
 public sealed class DataContext : Microsoft.EntityFrameworkCore.DbContext
 {
     public DbSet<UserEntity> Users { get; set; } = null!;
-    public DbSet<UserConfigEntity> UserConfigs { get; set; } = null!;
     public DbSet<MusicEntity> Musics { get; set; } = null!;
     public DbSet<MyFavoriteEntity> MyFavorites { get; set; } = null!;
-    public DbSet<LogEntity> Logs { get; set; } = null!;
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
     }
