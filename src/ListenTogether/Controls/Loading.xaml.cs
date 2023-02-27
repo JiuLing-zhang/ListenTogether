00947dcd869a48315ef4ace86d66fa236b9e15af
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2023-02-27 22:27:37 +0800

    优化 Loading 页的样式

--- src/ListenTogether/Controls/Loading.xaml.cs
+++ src/ListenTogether/Controls/Loading.xaml.cs
@@ -7,6 +7,10 @@ public partial class LoadingPage : Popup
     public LoadingPage(string message)
     {
         InitializeComponent();
+
+        double width = App.Current?.MainPage?.Window.Width ?? 300;
+        double height = App.Current?.MainPage?.Window.Height ?? 150;
+        this.Size = new Size(width, height);
         LblMessage.Text = message;
     }
 }
