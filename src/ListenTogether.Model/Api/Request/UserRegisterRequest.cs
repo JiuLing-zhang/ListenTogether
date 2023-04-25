f4d100922a321535da8260cf380b53787f59c84d
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2023-04-25 21:13:55 +0800

    增加数据长度校验

--- src/ListenTogether.Model/Api/Request/UserRegisterRequest.cs
+++ src/ListenTogether.Model/Api/Request/UserRegisterRequest.cs
@@ -5,12 +5,18 @@ namespace ListenTogether.Model.Api.Request;
 public class UserRegisterRequest
 {
     [Required]
+    [MinLength(4, ErrorMessage = "用户名长度至少4位")]
+    [MaxLength(20, ErrorMessage = "用户名长度最多20位")]
     public string Username { get; set; } = null!;
 
     [Required]
+    [MinLength(4, ErrorMessage = "昵称长度至少4位")]
+    [MaxLength(20, ErrorMessage = "昵称长度最多20位")]
     public string Nickname { get; set; } = null!;
 
     [Required]
+    [MinLength(4, ErrorMessage = "密码长度至少4位")]
+    [MaxLength(20, ErrorMessage = "密码长度最多20位")]
     public string Password { get; set; } = null!;
 
 }
--- src/ListenTogether.Model/Api/Request/UserRequest.cs
+++ src/ListenTogether.Model/Api/Request/UserRequest.cs
@@ -5,9 +5,13 @@ namespace ListenTogether.Model.Api.Request
     public class UserRequest
     {
         [Required]
+        [MinLength(4, ErrorMessage = "用户名长度至少4位")]
+        [MaxLength(20, ErrorMessage = "用户名长度最多20位")]
         public string Username { get; set; } = null!;
 
         [Required]
+        [MinLength(4, ErrorMessage = "密码长度至少4位")]
+        [MaxLength(20, ErrorMessage = "密码长度最多20位")]
         public string Password { get; set; } = null!;
     }
 }
