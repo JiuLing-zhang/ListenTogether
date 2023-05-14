c951ea31cd513b4ee17fb3757f71c0c52b125821
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2023-05-14 19:34:51 +0800

    歌词滚动

---
+++ src/ListenTogether.Pages/Lyric.cs
@@ -0,0 +1,14 @@
+﻿namespace ListenTogether.Pages;
+internal class Lyric
+{
+    public long PositionMillisecond { get; set; }
+    public string Info { get; set; }
+    public bool IsHighlight { get; set; }
+
+    public Lyric(long positionMillisecond, string info)
+    {
+        PositionMillisecond = positionMillisecond;
+        Info = info;
+        IsHighlight = false;
+    }
+}
